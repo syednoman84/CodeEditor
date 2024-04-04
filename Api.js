ece921511dd09aeb132d22e48bd079c237db3f65
Noman Ahmed <noman.ahmed@linearft.com>
2024-04-04 18:10:47 -0400

    updates

--- Api.js
+++ Api.js
@@ -20,6 +20,44 @@ app.get("/", function (req, res) {
   res.sendFile("C:/Noman/code/github/CodeEditor/index.html");
 });
 
+app.get("/questions", function (req, res) {
+  var fs = require("fs");
+
+  // reads the questions.json file
+  var questions = JSON.parse(fs.readFileSync("./questions.json"));
+
+  // returns one question randomly
+  res.send(questions[Math.floor(Math.random() * questions.length)]);
+
+  // let questions = [
+  //   {
+  //     question: "Inside which HTML element do we put the JavaScript??",
+  //     choice1: "<script>",
+  //     choice2: "<javascript>",
+  //     choice3: "<js>",
+  //     choice4: "<scripting>",
+  //     answer: 1,
+  //   },
+  //   {
+  //     question:
+  //       "What is the correct syntax for referring to an external script called 'xxx.js'?",
+  //     choice1: "<script href='xxx.js'>",
+  //     choice2: "<script name='xxx.js'>",
+  //     choice3: "<script src='xxx.js'>",
+  //     choice4: "<script file='xxx.js'>",
+  //     answer: 3,
+  //   },
+  //   {
+  //     question: " How do you write 'Hello World' in an alert box?",
+  //     choice1: "msgBox('Hello World');",
+  //     choice2: "alertBox('Hello World');",
+  //     choice3: "msg('Hello World');",
+  //     choice4: "alert('Hello World');",
+  //     answer: 4,
+  //   },
+  // ];
+});
+
 app.post("/compile", function (req, res) {
   var code = req.body.code;
   var input = req.body.input;
@@ -58,6 +96,7 @@ app.post("/compile", function (req, res) {
         var envData = { OS: "windows" };
         compiler.compileJava(envData, code, function (data) {
           if (data.output) {
+            console.log(data);
             res.send(data);
           } else {
             res.send({ output: "error" });
@@ -69,6 +108,7 @@ app.post("/compile", function (req, res) {
         //else
         compiler.compileJavaWithInput(envData, code, input, function (data) {
           if (data.output) {
+            console.log(data);
             res.send(data);
           } else {
             res.send({ output: "error" });
